47b899aa28918565f4263ba6075801f67154bc82
Chris Sembritzki <chris.strauch98@web.de>
2018-11-04 00:30:22 +0100

    added JSONCreator

--- project/src/fileWriter/JSONCreator.java
+++ project/src/fileWriter/JSONCreator.java
@@ -2,7 +2,21 @@ package fileWriter;
 
 public class JSONCreator {
 
-    // TODO: static methods to create JSON from Java Objects
-    // @ChrisSembritzki assigned
+    public JSONCreator(String path, Object object) {
+
+//        Gson gson = new GsonBuilder().create();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();   // Formatiere Ausgabe
+
+        String json = gson.toJson(object);
+//        System.out.println(json);
+
+
+        try (FileWriter writer = new FileWriter(path)) {
+            gson.toJson(object, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
